88ffc8a9ababe51cbc21a65ecd60300884dbd19e
spartak.gareginyan <gareginyans@mail.ru>
2025-03-19 07:45:52 +0400

    init.

---
+++ src/main/java/com/xcoder/kafka/config/KafkaProducerConfig.java
@@ -0,0 +1,21 @@
+package com.xcoder.kafka.config;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class KafkaProducerConfig {
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
+
+    public Map<String, Object> producerConfigs() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+
+        return props;
+    }
+}
